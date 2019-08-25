000d3b355705d7bd0c4bb477a904597b09465a5a
Lukasz Tarala <taralalukasz123@gmail.com>
2019-08-25 10:21:03 +0200

    added creation date to models

--- src/main/java/budgetapp/model/Expense.java
+++ src/main/java/budgetapp/model/Expense.java
@@ -2,12 +2,16 @@ package budgetapp.model;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import java.util.Date;
 
 @Data
 @NoArgsConstructor
@@ -24,4 +28,9 @@ public class Expense {
 
     @Column(name="name")
     private String name;
+
+    @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "create_date")
+    private Date creationDate;
 }
